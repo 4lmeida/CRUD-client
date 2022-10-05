9c33b0f98b01056c2f357658decd488271d78592
Luis Almeida <lferreira646@gmail.com>
2022-10-05 19:37:07 -0300

    GET Client paged

--- src/main/java/com/desafio/CRUDclientes/controllers/ClientController.java
+++ src/main/java/com/desafio/CRUDclientes/controllers/ClientController.java
@@ -1,6 +1,8 @@
 package com.desafio.CRUDclientes.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,11 @@ public class ClientController {
 	public ClientDTO findById(@PathVariable Long id) {
 		ClientDTO dto = service.findById(id);
 		return dto;
+	}
+	
+	@GetMapping
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
 	}
 	
 }
